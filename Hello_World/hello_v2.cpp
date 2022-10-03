3da0f0a6e53538d908a0c34720ee3a5e7043412d
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2022-10-03 03:13:55 -0400

    uses printf() instead of cout

---
+++ Hello_World/hello_v2.cpp
@@ -0,0 +1,10 @@
+// Simple hello world program for C++ by K0K0$H@
+// Uses printf()
+// run on Linux with g++ ./hello.cpp && ./a.out
+#include <iostream>
+using namespace std;
+
+int main(){
+        printf("Hello World\n");
+        return 0;
+}
