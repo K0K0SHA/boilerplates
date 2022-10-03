b53a76e6d34cb6461ad5e60bf699f2ca0be687da
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2022-10-03 06:43:58 -0400

    Update hello.java
    added run instructions

--- Hello_World/hello.java
+++ Hello_World/hello.java
@@ -1,4 +1,5 @@
-// simple Java Hello World by K0K0$HA
+// simple Java Hello World by K0K0$HA from GitHub
+// run with javac ./hello.java && java hello
 class hello{
 	public static void main(String[] args){
 		System.out.println("Hello World\n");
