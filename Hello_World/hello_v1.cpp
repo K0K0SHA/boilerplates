b06db0e5b449b605c1737ced892a8bdd4fe181b8
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2022-10-03 03:06:34 -0400

    Create hello_v1.cpp
    Pay attention how CPP has different ways to print something to terminal than C. Notice how this code differs from hello.c

---
+++ Hello_World/hello_v1.cpp
@@ -0,0 +1,11 @@
+// Simple hello world program for C++ by K0K0$H@
+// Uses cout << redirection
+// run on Linux with g++ ./hello.cpp && ./a.out
+// several other versions of this exist 
+#include <iostream>
+using namespace std;
+
+int main(){
+        cout << "Hello World\n";
+        return 0;
+}
