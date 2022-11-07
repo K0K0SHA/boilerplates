50eb2c1042e01b3aca1625c83179cd7cdb4b91cd
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2022-11-07 03:43:05 -0500

    Updated to compile with g++
    G++ has gcc built-in.

--- C/evil_compile.sh
+++ C/evil_compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # filename: evil_compile.sh
 # purpose: compiles ./evil_code.c into ./Chrome_Setup.exe
-gcc "./evil_code.c" -o "./Chrome_Setup.exe"
+g++ "./evil_code.c" -o "./Chrome_Setup.exe"
 
 # notes:
 # payloads go in evil_code.c
