eec3bf723382c939dfaaef9c3b5653e657db49e7
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2022-11-07 03:37:29 -0500

    Update compile_run_custom_executable_name.py

--- C/compile_run_custom_executable_name.py
+++ C/compile_run_custom_executable_name.py
@@ -2,6 +2,8 @@ import os
 import miscX as X
 # filename: compile_run_custom_executable_name.py
 # file purpose: cross-platform gcc compilation
+# file version: 0.2 
+# X.getString("Setup Name")
 commandstr = "gcc ./evil_code.c -o Chrome_Setup.exe" # shell code. gcc -o is file output option. 
 X.verbosity(commandstr)
 # dependency-less unsafe alternative (uncomment next line, comment previous):
