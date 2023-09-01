3846eafe5a038f98d914e9f3f41d325b4661a343
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2023-09-01 09:09:07 -0400

    Create compile_run_C_g++.sh
    compile and run C source code with g++

---
+++ C/compile_run_C_g++.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# compiles demo.c and runs it on Linux system using g++
+# basic example script; will not check dependencies
+# you should use try to use g++ instead of gcc
+g++ demo.c && ./a.out
