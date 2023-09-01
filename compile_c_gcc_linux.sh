d9dbbdf32144cf5cda815d18c9b989152664cfd4
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2023-09-01 09:05:40 -0400

    Update compile_c_gcc_linux.sh
    changed #!/bin/bash to #!/usr/bin/env bash

--- compile_c_gcc_linux.sh
+++ compile_c_gcc_linux.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # compiles demo.c and runs it on Linux system using gcc
 # basic example script; will not check dependencies
 # if you have g++, you should use that instead
