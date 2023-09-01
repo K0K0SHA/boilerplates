0d7d60fcf081e5a6c20ab776148734aa8400345e
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2023-09-01 09:04:44 -0400

    Update and rename compile_c.sh to compile_c_gcc_linux.sh
    updated comments

--- compile_c.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# compiles demo.c and runs it on Linux system using gcc
-# basic script; will not check dependencies
-gcc demo.c && ./a.out
---
+++ compile_c_gcc_linux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# compiles demo.c and runs it on Linux system using gcc
+# basic example script; will not check dependencies
+# if you have g++, you should use that instead
+gcc demo.c && ./a.out
