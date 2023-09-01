cea8a1355de1f846bbb8044e1dc061faab6e65b1
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2023-09-01 09:16:22 -0400

    Update evil_compile.sh
    cross platform compat todo

--- C/evil_compile.sh
+++ C/evil_compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # filename: evil_compile.sh
 # purpose: compiles ./evil_code.c into ./Chrome_Setup.exe
+# TODO: experiment with getting rid of ./ for cross-platform compat
 g++ "./evil_code.c" -o "./Chrome_Setup.exe"
 
 # notes:
