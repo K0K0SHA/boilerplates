163408d721b3b9bf429ff4cb391e27a358bd21c1
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2024-08-02 23:32:55 -0400

    Update compile_c_gcc_linux.sh
    updated comments

--- compile_c_gcc_linux.sh
+++ compile_c_gcc_linux.sh
@@ -2,4 +2,5 @@
 # compiles demo.c and runs it on Linux system using gcc
 # basic example script; will not check dependencies
 # if you have g++, you should use that instead
+# this will not build programs which require make
 gcc demo.c && ./a.out
