277893d10060211a97a2a907dce050a48fce0ca0
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2025-12-15 05:00:11 -0500

    Create Hello_World.java
    Hello World in Java

---
+++ Java/Hello_World.java
@@ -0,0 +1,10 @@
+// filename: Hello_World.java
+// how to run on Linux/Mac:
+// javac Hello_World.java && java Hello_World
+// how to run on Windows:
+// javac Hello_World.java ; java Hello_World
+public class Hello_World{
+        public static void main(String[] args){
+                System.out.println("Hello World");
+        }
+}
